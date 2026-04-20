dc80c91cec648e6edd5301203a8f96898591bc8f
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2026-04-20 10:27:19 +0700

    fisrt upload

---
+++ praktikum7kedua/src/test/java/com/example/praktikum7kedua/Praktikum7keduaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.praktikum7kedua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Praktikum7keduaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
