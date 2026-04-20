28b50f65b9c5147e2ccac6033a0ba530f371230a
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2026-04-20 10:33:41 +0700

    menambah user controller api

---
+++ praktikum7kedua/src/main/java/com/example/praktikum7kedua/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.example.praktikum7kedua.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+    private final UserService userService;
+}
