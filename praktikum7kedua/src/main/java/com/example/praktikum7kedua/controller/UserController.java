030451815da7681d8ab261d18f98c51dc6930ba2
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2026-04-20 10:36:58 +0700

    menambah endpoint untuk create user

--- praktikum7kedua/src/main/java/com/example/praktikum7kedua/controller/UserController.java
+++ praktikum7kedua/src/main/java/com/example/praktikum7kedua/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.praktikum7kedua.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/users")
 public class UserController {
     private final UserService userService;
+    
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public String createUser(@RequestBody User request) {
+        userService.addUser(request);
+        return "user created successfuly";
+    }
 }
