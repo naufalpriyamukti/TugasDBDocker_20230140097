e6e340246499d556c5554ce03b2f5c011cc1f80d
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2026-04-20 10:29:56 +0700

    menambah entitas user

---
+++ praktikum7kedua/src/main/java/com/example/praktikum7kedua/model/User.java
@@ -0,0 +1,26 @@
+package com.example.praktikum7kedua.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "20230140097_users")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    private String  id;
+
+    @Column(nullable =false)
+    private String name;
+
+    @Column(nullable = false)
+    private String nim;
+}
