b5661c881df242b9f10b11195668341211d18d75
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2026-04-20 10:46:40 +0700

    menambah user repository

---
+++ praktikum7kedua/src/main/java/com/example/praktikum7kedua/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.praktikum7kedua.repository;
+
+import com.example.praktikum7kedua.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository extends JpaRepository<User, String> {
+}
